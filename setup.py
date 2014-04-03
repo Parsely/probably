647d2da34af7b0404e2aee72349222a8843b10c5
Martin Laprise <mlaprise@gmail.com>
2014-04-03 13:41:34 -0400

    add smhasher in setup req

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     packages=find_packages(exclude=['ez_setup']),
     platforms=['any'],
     zip_safe=False,
-    install_requires=['numpy', 'cython', 'bitarray'],
+    install_requires=['numpy', 'cython', 'bitarray', 'smhasher'],
     ext_modules = [Extension("maintenance", ["pds/maintenance.c"], include_dirs=[numpy.get_include()])],
 )
