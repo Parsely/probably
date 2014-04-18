728ab505cfd9ce89662eb2d1075dc5d6a1c4aee6
Martin Laprise <mlaprise@gmail.com>
2014-04-18 15:50:32 -0400

    Add DailyTemporalBase

--- pds/bloomfilter_cython.pyx
+++ pds/bloomfilter_cython.pyx
@@ -22,6 +22,98 @@ cdef extern from "MurmurHash2A.h" nogil:
     unsigned int MurmurHash2A (void * key, int len, unsigned int seed)
 
 
+cdef class DailyTemporalBase(BloomFilter):
+
+    cdef unsigned char *current_day_bitarray
+
+    def __cinit__(self, capacity, error_rate):
+        self.capacity = capacity
+        self.error_rate = error_rate
+        self._initialize_parameters()
+        self._count = 0
+
+        self.current_day_bitarray = <unsigned char*> PyMem_Malloc(self.nbr_bytes * sizeof(unsigned char))
+        self.bitarray = <unsigned char*> PyMem_Malloc(self.nbr_bytes * sizeof(unsigned char))
+        if not (self.bitarray and self.current_day_bitarray):
+            raise MemoryError("Unable to allocate memory for BloomFilter")
+        else:
+            self._initialize_bitarray()
+
+    @cython.wraparound(False)
+    @cython.boundscheck(False)
+    @cython.cdivision(True)
+    cdef int __check_or_add(self, const char *value, int should_add=1):
+        cdef int hits = 0
+        cdef int val_len = len(value)
+        #cdef unsigned long a
+        #cdef unsigned long b
+        cdef unsigned int a = MurmurHash2A(value, val_len, 0x9747b28c)
+        cdef unsigned int b = MurmurHash2A(value, val_len, a)
+        cdef unsigned int x
+        cdef unsigned int i
+        cdef unsigned int byte
+        cdef unsigned int mask
+        cdef unsigned char c
+
+        #MurmurHash3_x86_32(<char*> value, val_len, 0x9747b28c, &a)
+        #MurmurHash3_x86_32(<char*> value, val_len, a, &b)
+
+        for i in range(self.nbr_slices):
+            x = (a + i * b) % self.nbr_bits
+            byte = x >> 3
+            c = self.bitarray[byte]
+            mask = 1 << (x % 8)
+
+            if c & mask:
+                hits += 1
+            else:
+                if should_add == 1:
+                    self.bitarray[byte] = c | mask
+                    self.current_day_bitarray[byte] = c | mask
+
+        if hits == self.nbr_slices:
+            return 1
+
+        if should_add:
+            self._count += 1
+
+        return 0
+
+    def _save_snapshot(self,  char* filename, current=True):
+        f = fopen(filename, 'w')
+        if current:
+            fwrite(self.current_day_bitarray, 1, self.nbr_bytes, f)
+        else:
+            fwrite(self.bitarray, 1, self.nbr_bytes, f)
+        fclose(f)
+
+    def _union_bf_from_file(self, char* filename, current=False):
+        temp_bitarray = <unsigned char*> PyMem_Malloc(self.nbr_bytes * sizeof(unsigned char))
+
+        f = fopen(filename, "r")
+        fread(temp_bitarray, 1, self.nbr_bytes, f)
+        fclose(f)
+
+        if current:
+            self._bitarray_or(self.current_day_bitarray, temp_bitarray)
+        else:
+            self._bitarray_or(self.bitarray, temp_bitarray)
+        PyMem_Free(temp_bitarray)
+
+    @cython.wraparound(False)
+    @cython.boundscheck(False)
+    @cython.cdivision(True)
+    cdef void _initialize_bitarray(self):
+        for i in range(self.nbr_bytes):
+            self.bitarray[i] = 0
+            self.current_day_bitarray[i] = 0
+
+    def __dealloc__(self):
+        # The __dealloc__ of the superclass is always call
+        # So bitarray will be dealloc there
+        PyMem_Free(self.current_day_bitarray)
+
+
 cdef class BloomFilter:
 
     cdef unsigned int nbr_slices
@@ -39,6 +131,11 @@ cdef class BloomFilter:
         self.error_rate = error_rate
         self._initialize_parameters()
         self._count = 0
+        self.bitarray = <unsigned char*> PyMem_Malloc(self.nbr_bytes * sizeof(unsigned char))
+        if not self.bitarray:
+            raise MemoryError("Unable to allocate memory for BloomFilter")
+        else:
+            self._initialize_bitarray()
 
     def _initialize_parameters(self):
         self.nbr_slices = int(math.floor(math.log(1.0 / self.error_rate, 2.0)))
@@ -56,35 +153,36 @@ cdef class BloomFilter:
             self.nbr_bytes = (self.nbr_bits / 8) + 1
         else:
             self.nbr_bytes = self.nbr_bits / 8
-        self.bitarray = <unsigned char*> PyMem_Malloc(self.nbr_bytes * sizeof(unsigned char))
-        if not self.bitarray:
-            raise MemoryError("Unable to allocate memory for BloomFilter")
-        else:
-            self._initialize_bitarray()
 
     def _set_capacity(self, capacity):
         self.capacity = capacity
 
-    def save_snapshot(self,  char* filename):
+    def _set_error_rate(self, error_rate):
+        self.error_rate = error_rate
+
+    def _save_snapshot(self,  char* filename):
         f = fopen(filename, 'w')
         fwrite(self.bitarray, 1, self.nbr_bytes, f)
         fclose(f)
 
-    def read_snapshot(self, char* filename):
+    def _read_snapshot(self, char* filename):
         f = fopen(filename, "r")
         fread(self.bitarray, 1, self.nbr_bytes, f)
         fclose(f)
 
+    def _union_bf_from_file(self, char* filename):
+        temp_bitarray = <unsigned char*> PyMem_Malloc(self.nbr_bytes * sizeof(unsigned char))
+
+        f = fopen(filename, "r")
+        fread(temp_bitarray, 1, self.nbr_bytes, f)
+        fclose(f)
+
+        self._bitarray_or(self.bitarray, temp_bitarray)
+        PyMem_Free(temp_bitarray)
+
     def initialize_bitarray(self):
         self._count = 0
         self._initialize_bitarray()
-
-    cdef void _bitarray_or(self, unsigned char* other_bitarray):
-        for i in range(self.nbr_bytes):
-            self.bitarray[i] = self.bitarray[i] | other_bitarray[i]
-
-    def union(self, BloomFilter other):
-        self._bitarray_or(other.bitarray)
 
     @cython.wraparound(False)
     @cython.boundscheck(False)
@@ -93,12 +191,16 @@ cdef class BloomFilter:
         for i in range(self.nbr_bytes):
             self.bitarray[i] = 0
 
+    cdef void _bitarray_or(self, unsigned char* bitarray, unsigned char* other_bitarray):
+        for i in range(self.nbr_bytes):
+            bitarray[i] = bitarray[i] | other_bitarray[i]
+
+    def union(self, BloomFilter other):
+        self._bitarray_or(self.bitarray, other.bitarray)
+
     def __repr__(self):
-        return """ Capacity: %s
-                   Error rate: %s
-                   nbr. bits: %s
-                   nbr. bytes: %s
-                   nbr. hashes: %s """ % (self.capacity, self.error_rate, self.nbr_bits, self.nbr_bytes, self.nbr_slices)
+        return """Capacity: %s\nError rate: %s\nnbr. bits: %s\nnbr. bytes: %s\nnbr. hashes: %s """ \
+                % (self.capacity, self.error_rate, self.nbr_bits, self.nbr_bytes, self.nbr_slices)
 
     cdef int _get_bit(self, unsigned long index):
         bytepos, bitpos = divmod(index, 8)
@@ -172,4 +274,4 @@ cdef class BloomFilter:
         return self.contains(value)
 
     def __dealloc__(self):
-            PyMem_Free(self.bitarray)
+        PyMem_Free(self.bitarray)
