21d12e891670f49ddd28ad0d1883bed08ce3a173
Martin Laprise <mlaprise@gmail.com>
2014-04-18 15:51:04 -0400

    update snapshot during rebuild

--- pds/temporal_daily_cython.py
+++ pds/temporal_daily_cython.py
@@ -9,14 +9,14 @@ import zlib
 import bitarray
 import numpy as np
 
-from bloomfilter_cython import BloomFilter
+from bloomfilter_cython import BloomFilter, DailyTemporalBase
 from pycassa import NotFoundException
 from pycassa.pool import ConnectionPool
 from pycassa.system_manager import SystemManager, SIMPLE_STRATEGY
 from pycassa.columnfamily import ColumnFamily
 
 
-class DailyTemporalBloomFilter(BloomFilter):
+class DailyTemporalBloomFilter(DailyTemporalBase):
     """Long Range Temporal BloomFilter using a daily resolution.
 
     For really high value of expiration (like 60 days) with low requirement on precision.
@@ -29,10 +29,10 @@ class DailyTemporalBloomFilter(BloomFilter):
     items of the set are uniformly distributed over time, the avg error will be something like 1.0 / expiration
     """
 
-    def __new__(cls, capacity, error_rate, expiration, name, cassandra_session):
+    def __new__(cls, capacity, error_rate, expiration, name, cassandra_session, snapshot_path='./'):
         return super(DailyTemporalBloomFilter, cls).__new__(cls, capacity=capacity, error_rate=error_rate)
 
-    def __init__(self, capacity, error_rate, expiration, name, cassandra_session):
+    def __init__(self, capacity, error_rate, expiration, name, cassandra_session, snapshot_path='./'):
         filename = ""
         super(DailyTemporalBloomFilter, self).__init__(capacity=capacity, error_rate=error_rate)
         self.bf_name = name
@@ -45,6 +45,7 @@ class DailyTemporalBloomFilter(BloomFilter):
         self.commit_batch = 1000
         self.columnfamily = None
         self.ensure_cassandra_cf()
+        self.snapshot_path = snapshot_path
 
     def ensure_cassandra_cf(self):
         s = SystemManager()
@@ -77,6 +78,18 @@ class DailyTemporalBloomFilter(BloomFilter):
             else:
                 yield start + dt.timedelta(hours=i)
 
+    def _day_range(self, start, end, reverse=False, inclusive=True):
+        """Generator that gives us all the days between a start and end datetime
+        (inclusive)."""
+        days = (end - start).days
+        if inclusive:
+            days += 1
+        for i in xrange(days):
+            if reverse:
+                yield end - dt.timedelta(days=i)
+            else:
+                yield start + dt.timedelta(days=i)
+
     def _drop_archive(self):
         last_period = self.current_period - dt.timedelta(days=self.expiration-1)
         hours = self._hour_range(last_period, dt.datetime.now())
@@ -88,20 +101,52 @@ class DailyTemporalBloomFilter(BloomFilter):
             except:
                 pass
 
-    def rebuild_from_archive(self):
+    def rebuild_from_archive(self, rebuild_snapshot=False):
         """Rebuild the BF using the archived items"""
-        #self.initialize_bitarray()
+        self.initialize_bitarray()
+
+        #if rebuild_snapshot:
+        #    self.delete_snapshots()
+
+        def multi_rows_itr(rows):
+            for row in rows.values():
+                for k in row.keys():
+                    yield k
+
         last_period = self.current_period - dt.timedelta(days=self.expiration-1)
         hours = self._hour_range(last_period, dt.datetime.now())
+        days = self._day_range(last_period, dt.datetime.now())
         rows = []
-        for i,hour in enumerate(hours):
-            row = "%s_%s" % (self.bf_name, hour.strftime('%Y-%m-%d:%H'))
-            rows.append(row)
-        rows_content = self.columnfamily.multiget(rows, column_count=1E6)
+        for i,day in enumerate(days):
+            rows = ["%s_%s:%s" % (self.bf_name, day.strftime('%Y-%m-%d'), hour_str) for hour_str in ["%02d" % i for i in range(24)]]
+            rows_content = self.columnfamily.multiget(rows, column_count=1E6)
 
-        for row_content in rows_content.values():
-            for k in row_content.keys():
+            for k in multi_rows_itr(rows_content):
                 self.add_rebuild(k)
+
+            if rebuild_snapshot:
+                self.save_snaphot(override_period=day)
+
+    def restore_from_disk(self, clean_old_snapshot=False):
+        """Restore the state of the BF using previous snapshots.
+
+        :clean_old_snapshot: Delete the old snapshot on the disk (period < current - expiration)
+        """
+        base_filename = "%s/%s_%s_*.dat" % (self.snapshot_path, self.bf_name, self.expiration)
+        availables_snapshots = glob.glob(base_filename)
+        last_period = self.current_period - dt.timedelta(days=self.expiration-1)
+        for filename in availables_snapshots:
+            snapshot_period = dt.datetime.strptime(filename.split('_')[-1].strip('.dat'), "%Y-%m-%d")
+            if snapshot_period <  last_period and not clean_old_snapshot:
+                continue
+            else:
+                self._union_bf_from_file(filename)
+                if snapshot_period == self.current_period:
+                    self._union_bf_from_file(filename, current=True)
+
+            if snapshot_period < last_period and clean_old_snapshot:
+                os.remove(filename)
+        self.ready = True
 
     def add_rebuild(self, key):
         super(DailyTemporalBloomFilter, self).add(key)
@@ -117,11 +162,12 @@ class DailyTemporalBloomFilter(BloomFilter):
 
         return result
 
-    def resize(self, new_capacity):
-        self._set_capacity(new_capacity)
+    def resize(self, new_capacity=None, new_error_rate=None):
+        self._set_capacity(new_capacity or self.capacity)
+        self._set_error_rate(new_error_rate or self.error_rate)
         self._initialize_parameters()
         self.initialize_bitarray()
-        self.rebuild_from_archive()
+        self.rebuild_from_archive(rebuild_snapshot=True)
 
     def initialize_period(self, period=None):
         """Initialize the period of BF.
@@ -135,3 +181,12 @@ class DailyTemporalBloomFilter(BloomFilter):
         self.current_period = dt.datetime(self.current_period.year, self.current_period.month, self.current_period.day)
         self.date = self.current_period.strftime("%Y-%m-%d")
 
+    def save_snaphot(self, override_period=None):
+        """Save the current state of the current day bitarray on disk.
+
+        Save the internal representation (bitarray) into a binary file using this format:
+            filename : name_expiration_2013-01-01.dat
+        """
+        period = override_period or self.current_period
+        filename = "%s/%s_%s_%s.dat" % (self.snapshot_path, self.bf_name, self.expiration, period.strftime("%Y-%m-%d"))
+        self._save_snapshot(filename)
