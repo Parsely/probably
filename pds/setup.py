4a58d982721ff850ca3af76639a9d7894c9e0fda
Martin Laprise <mlaprise@gmail.com>
2014-04-22 10:48:45 -0400

    Update setup.py

--- pds/setup.py
+++ pds/setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
 import numpy
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("maintenance", ["maintenance.pyx"], include_dirs=[numpy.get_include()]),]
+    ext_modules = [Extension("bloomfilter_cython", ["bloomfilter_cython.pyx", "MurmurHash2A.c", "MurmurHash3.cpp"]),
+                   Extension("maintenance", ["maintenance.pyx"], include_dirs=[numpy.get_include()]),
+    ]
 )
+
+
---
+++ pds/test_bf.py
@@ -0,0 +1,49 @@
+import time
+import numpy as np
+import pandas as pd
+import itertools
+
+from bloomfilter_cython import BloomFilter
+#from pybloomfilter import BloomFilter
+
+def test_error_rate(bf, capacity, nbr_experiments):
+    # uuid for the poor (faster them uuid.uuid4())
+    random_items = [str(r) + str(time.time()) for r in np.random.randn(2*capacity)]
+    for i in range(nbr_experiments):
+        bf.initialize_bitarray()
+        for item in random_items[:capacity]:
+            bf.add(item)
+
+        false_positive = 0
+        for item in random_items[capacity:2*capacity]:
+            if item in bf:
+                false_positive += 1
+
+        false_positive_rate = float(false_positive) / capacity
+
+        false_negative = 0
+        for item in random_items[:capacity]:
+            if not item in bf:
+                false_negative += 1
+
+        false_negative_rate = float(false_negative) / capacity
+        yield false_negative_rate, false_positive_rate
+
+
+if __name__ == "__main__":
+    nbr_experiments = 100
+    capacities = np.logspace(2,5,4)
+    #error_rates = [0.001, 0.01, 0.02, 0.05, 0.1, 0.5]
+    error_rates = [0.02]
+    all_results = []
+
+    for capacity, error_rate in itertools.product(capacities, error_rates):
+        print "Testing capacity=%s and error_rate=%s" % (capacity, error_rate)
+        bf = BloomFilter(int(capacity), error_rate)
+        error_rate_result = pd.DataFrame(test_error_rate(bf, int(capacity), nbr_experiments), columns=['false_negative','false_positive'])
+        all_results.append((capacity,
+                            error_rate,
+                            error_rate_result.false_positive.mean(),
+                            error_rate_result.false_negative.mean()))
+
+    all_results_df = pd.DataFrame(all_results, columns=['capacity', 'error_rate', 'false_positive', 'false_negative'])
