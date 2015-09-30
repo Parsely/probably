14722c028352cbea358dcc219908f4ca5fefef88
Dan Blanchard <dan.blanchard@gmail.com>
2015-09-30 09:27:34 -0400

    Fix issues with bootstrapping numpy in setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python
-from ez_setup import use_setuptools
-use_setuptools()
-
-import os
-
 from setuptools import setup, find_packages, Extension
-from distutils.command import build_ext
-import numpy
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+class build_ext(_build_ext):
+    """
+    This class is necessary because numpy won't be installed at import time.
+    """
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
 
 VERSION = '0.0.1'
 DESCRIPTION = "PDS: Simple Probabilistic Data Structure"
-LONG_DESCRIPTION = """
-"""
-
-CLASSIFIERS = filter(None, map(str.strip,
-"""
-Intended Audience :: Developers
-License :: OSI Approved :: MIT License
-Programming Language :: Python
-Operating System :: OS Independent
-Topic :: Utilities
-Topic :: Database :: Database Engines/Servers
-Topic :: Software Development :: Libraries :: Python Modules
-""".splitlines()))
+LONG_DESCRIPTION = ""
+CLASSIFIERS = ['Intended Audience :: Developers',
+               'License :: OSI Approved :: MIT License'
+               'Programming Language :: Python',
+               'Operating System :: OS Independent',
+               'Topic :: Utilities',
+               'Topic :: Database :: Database Engines/Servers',
+               'Topic :: Software Development :: Libraries :: Python Modules']
 
 setup(
     name="pds",
@@ -40,5 +42,7 @@ setup(
     platforms=['any'],
     zip_safe=False,
     install_requires=['numpy', 'cython', 'bitarray', 'smhasher'],
-    ext_modules = [Extension("maintenance", ["pds/maintenance.c"], include_dirs=[numpy.get_include()])],
+    setup_requires=['numpy'],
+    cmdclass={'build_ext':build_ext},
+    ext_modules=[Extension("maintenance", ["pds/maintenance.c"])],
 )
